4fbbaf2fee1077ec90e0d86eade8537bdb067e0f
Matthew Hope <chickazama@gmail.com>
2024-04-21 16:55:00 +0100

    Scaffold Client & Server structs

---
+++ Client.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ID         int
+	Connection net.Conn
+	Incoming   chan []byte
+	Outgoing   chan []byte
+	Server     *Server
+}
+
+func NewClient(id int, conn net.Conn) *Client {
+	ret := new(Client)
+	ret.ID = id
+	ret.Connection = conn
+	ret.Incoming = make(chan []byte)
+	ret.Outgoing = make(chan []byte)
+	return ret
+}
+
+// The Receive method reads in a null-terminated buffer
+// from the Client's TCP Connection, and sends the buffer
+// to the Incoming channel
+func (c *Client) Receive() error {
+	defer c.Connection.Close()
+	br := bufio.NewReader(c.Connection)
+	for {
+		buf, err := br.ReadBytes(0)
+		if err != nil {
+			return err
+		}
+		br.Reset(c.Connection)
+		c.Incoming <- buf
+	}
+}
+
+// The Send method looks continuously for contents
+// the Client's Outgoing channel, and writes them
+// to the Client's TCP connection.
+func (c *Client) Send() error {
+	defer c.Connection.Close()
+	for buf := range c.Outgoing {
+		_, err := c.Connection.Write(buf)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// The Forward method looks continuously for contents
+// in the Client's Outgoing channel, and forwards them
+// to the Server's broadcast channel
+// func (c *Client) Forward() {
+// 	for buf := range c.Incoming {
+// 		c.Server.Broadcast <- buf
+// 	}
+// }
+
+func (c *Client) HandleMessage() {
+	for buf := range c.Incoming {
+		fmt.Println(buf)
+		str := string(buf[:len(buf)-1])
+		switch str {
+		case "meow":
+			fmt.Println("CHEAT CODE ACTIVATED")
+		default:
+			c.Server.Broadcast <- buf
+		}
+	}
+}
---
+++ Server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+)
+
+var (
+	nextID = 1
+)
+
+type Server struct {
+	fp        *os.File
+	mtx       sync.Mutex
+	Clients   map[int]*Client
+	Listener  net.Listener
+	Broadcast chan []byte
+}
+
+func NewServer(network, addr string) *Server {
+	var err error
+	ret := new(Server)
+	ret.fp, err = os.OpenFile("history.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	ret.Clients = make(map[int]*Client)
+	ret.Broadcast = make(chan []byte, 8)
+	ret.Listener, err = net.Listen(network, addr)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return ret
+}
+
+func (s *Server) Start() {
+	go s.AcceptConnections()
+	go s.Send()
+}
+
+func (s *Server) AcceptConnections() error {
+	for {
+		conn, err := s.Listener.Accept()
+		if err != nil {
+			return err
+		}
+		s.mtx.Lock()
+		client := NewClient(nextID, conn)
+		client.Server = s
+		s.Clients[client.ID] = client
+		go client.Send()
+		go client.Receive()
+		go client.HandleMessage()
+		nextID++
+		s.mtx.Unlock()
+	}
+}
+
+func (s *Server) Send() {
+	for buf := range s.Broadcast {
+		s.mtx.Lock()
+		str := string(buf[:len(buf)-1])
+		fmt.Fprintf(s.fp, "%s\n", str)
+		for _, c := range s.Clients {
+			c.Outgoing <- buf
+		}
+		s.mtx.Unlock()
+	}
+}
